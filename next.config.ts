9b43a225c1a93b6db2f741934398f55ddd624c9d
Shani <shanis@z-school.co.il>
2026-06-25 12:29:58 +0300

    Include content folder in Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  outputFileTracingIncludes: {
+    "/": ["./content/**/*"],
+    "/units/[slug]": ["./content/**/*"],
+  },
 };
 
 export default nextConfig;
